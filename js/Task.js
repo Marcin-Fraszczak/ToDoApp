36f96791ae0697864d458c38a77f4192f5570da7
Marcin-Fraszczak <kolothomasa@wp.pl>
2023-03-12 20:01:02 +0100

    final version of an app

--- js/Task.js
+++ js/Task.js
@@ -15,9 +15,9 @@ const Task = (props) => {
 
     useEffect(() => {
         if (task.id !== props.task.id) {
-            // setTaskReload(!taskReload)
             setTask(props.task)
-            // getOperations(props.task.id, setOperations)
+            setStatus(props.task.status)
+            setForm(false)
         }
     }, [props.task])
 
@@ -27,55 +27,58 @@ const Task = (props) => {
 
     const changeTaskStatus = (id, status) => {
         let data = {
-            title: props.task.title,
-            description: props.task.description,
+            title: task.title,
+            description: task.description,
             status: status
         }
         status === 'closed' && setForm(false)
         putTask(id, data, setStatus)
     }
 
-    return (
-        <section className="card mt-5 shadow-sm">
-            <div className="card-header d-flex justify-content-between align-items-center">
-                <div>
-                    <h5>{props.task.title}</h5>
-                    <h6 className="card-subtitle text-muted">{props.task.description}</h6>
+    if (task.id === props.task.id) {
+        return (
+            <section className="card mt-5 shadow-sm">
+                <div className="card-header d-flex justify-content-between align-items-center">
+                    <div>
+                        <h5>{task.title}</h5>
+                        <h6 className="card-subtitle text-muted">{task.description}</h6>
+                    </div>
+
+                    <div>
+                        <button className="btn btn-info btn-sm mr-2" hidden={status !== 'open'}
+                                onClick={() => setForm(!form)}>
+                            Add operation
+                            <i className="fas fa-plus-circle ml-1"></i>
+                        </button>
+
+                        <button className="btn btn-dark btn-sm" hidden={status !== 'open'}
+                                onClick={() => changeTaskStatus(task.id, 'closed')} style={blackButtonStyle}>
+                            Finish
+                            <i className="fas fa-archive ml-1"></i>
+                        </button>
+                        <button className="btn btn-dark btn-sm" hidden={status === 'open'}
+                                onClick={() => changeTaskStatus(task.id, "open")} style={blackButtonStyle}>
+                            Open
+                            <i className="fas fa-box-open ml-1"></i>
+                        </button>
+
+                        <button className="btn btn-outline-danger btn-sm ml-2" hidden={!operations}
+                                onClick={() => props.onRemoveTask(task.id)}>
+                            <i className="fas fa-trash false"></i>
+                        </button>
+                    </div>
                 </div>
 
-                <div>
-                    <button className="btn btn-info btn-sm mr-2" hidden={status !== 'open'}
-                            onClick={() => setForm(!form)}>
-                        Add operation
-                        <i className="fas fa-plus-circle ml-1"></i>
-                    </button>
+                <Operations taskId={task.id} form={form} taskReload={taskReload} setTaskReload={setTaskReload}>
 
-                    <button className="btn btn-dark btn-sm" hidden={status !== 'open'}
-                            onClick={() => changeTaskStatus(props.task.id, 'closed')} style={blackButtonStyle}>
-                        Finish
-                        <i className="fas fa-archive ml-1"></i>
-                    </button>
-                    <button className="btn btn-dark btn-sm" hidden={status === 'open'}
-                            onClick={() => changeTaskStatus(props.task.id, "open")} style={blackButtonStyle}>
-                        Open
-                        <i className="fas fa-box-open ml-1"></i>
-                    </button>
+                    {operations && operations.map((item, index) => <Operation
+                        key={index} operation={item} status={status} taskReload={taskReload}
+                        setTaskReload={setTaskReload}/>)}
 
-                    <button className="btn btn-outline-danger btn-sm ml-2" hidden={!operations}
-                            onClick={() => props.onRemoveTask(props.task.id)}>
-                        <i className="fas fa-trash false"></i>
-                    </button>
-                </div>
-            </div>
-
-            <Operations taskId={props.task.id} form={form} taskReload={taskReload} setTaskReload={setTaskReload}>
-
-                {operations && operations.map((item, index) => <Operation
-                    key={index} operation={item} status={status} taskReload={taskReload} setTaskReload={setTaskReload}/>)}
-
-            </Operations>
-        </section>
-    )
+                </Operations>
+            </section>
+        )
+    }
 }
 
 export {Task}
